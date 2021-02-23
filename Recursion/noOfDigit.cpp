093bc5db1d9bb9184b02e55105c53f7a6f70dd24
Nitish Diwakar <nitish7470918759@gmail.com>
2021-02-23 23:45:16 +0530

    Update noOfDigit.cpp

--- Recursion/noOfDigit.cpp
+++ Recursion/noOfDigit.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int num(int n, int c){
+/* int num(int n, int c){
     int d=n/10;
     c++;
     if(d==0){
@@ -16,5 +16,27 @@ int main() {
 	int x=0;
 	cin>>n;
 	cout<<num(n,x);
+	return 0;
+} */
+
+// no of digit using recursion
+
+int num(int n, int c){
+    int d=n/10;
+    c++;
+    if(d==0){
+        return c;
+    }
+    num(d,c);
+}
+
+int num(int n){
+    return num(n,0); // function overloading
+}
+
+int main() {
+	int n;
+	cin>>n;
+	cout<<num(n);
 	return 0;
 }
