a04dd097bc8fb29401b967c429afc9c85c0e4e07
Nitish Diwakar <nitish7470918759@gmail.com>
2021-02-23 23:54:55 +0530

    Create lastIndex.cpp

---
+++ Recursion/lastIndex.cpp
@@ -0,0 +1,17 @@
+// last index
+#include <iostream>
+using namespace std;
+
+int check(int a[], int s, int n){
+    if(s==0) return -1;
+    if(a[s-1]==n) return s-1;
+    check(a,s-1,n);
+}
+
+int main() {
+	int a[]={0,1,2,10,3,4,10,2,11};
+	int s = sizeof(a)/sizeof(a[0]);
+	num = 2;
+	cout<<check(a,s,num);
+	return 0;
+}
