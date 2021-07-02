e19bb680b333d661c42f222eeaf323433ca8b418
Nitish Diwakar <nitish7470918759@gmail.com>
2021-07-02 17:15:25 +0530

    Create diagonalDiff.cpp

---
+++ Hackrrank/diagonalDiff.cpp
@@ -0,0 +1,15 @@
+
+int diagonalDifference(vector<vector<int>> arr) {
+    int a=0,b=0,ans,x;
+    for(int i=0;i<arr.size();i++){
+        a += arr[i][i];
+    }
+    x=arr.size()-1;
+    for(int i=0;i<arr.size();i++){
+        b += arr[i][x];
+        x--;
+    }
+    ans=a-b;
+    if(ans<0) return -1*ans;
+    return ans;
+}
